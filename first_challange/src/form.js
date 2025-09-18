06d275655d7dac6e2f4a19aaede18142cc7ad2a4
PrimeSafar <dleerdleer515@gmail.com>
2025-09-18 17:00:21 +0300

    solu . of form change

--- first_challange/src/form.js
+++ first_challange/src/form.js
@@ -1,30 +1,42 @@
 import { useState } from "react";
 export default function Form() {
-//   const [nameInput, setNameInput] = useState("");
-//   const [emailInput,setEmailInput]= useState("")
-const [formInputs , setFormInputs] =useState({name:"",email:""})
+  //   const [nameInput, setNameInput] = useState("");
+  //   const [emailInput,setEmailInput]= useState("")
+  const [formInputs, setFormInputs] = useState({ name: "", email: "" });
 
   return (
-    <form onSubmit={(event)=>{
-        event.preventDefault()
-    }}>
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+      }}
+    >
       <label>name:</label>
       <input
         value={formInputs.name}
         onChange={(event) => {
-          setFormInputs({name:event.target.value});
+          //?   const newNameInput = { ...formInputs };
+          //*   newNameInput.name = event.target.value;
+          //!   setFormInputs({ newNameInput });
+          setFormInputs({ ...formInputs, name: event.target.value });
         }}
       />
       <hr></hr>
 
       <label>email:</label>
-      <input  value={formInputs.email}
+      <input
+        value={formInputs.email}
         onChange={(event) => {
-          setFormInputs({email:event.target.value});
-        }} />
+          //?   const newEmailFormInput = { ...formInputs };
+          //!!   newEmailFormInput.email = event.target.value;
+          //?   setFormInputs({ newEmailFormInput });
+          setFormInputs({ ...formInputs, email: event.target.value });
+        }}
+      />
 
       <hr></hr>
       <button type="submit">Submit</button>
     </form>
   );
 }
+
+//the object in js in pass by references so is like a pointer
